3dea9f444b26b2a84dc8a34ec09b0bfc55c669dd
Michael Kwan <michaelburps@gmail.com>
2019-02-24 00:04:57 -0600

    yeet

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    server.listen(process.env.PORT || 3000);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.loq(`Listening on ${ PORT }`));
 
 
 var easyNumSet = [];
